dc077bc64fdefed5f07288c17ec57667627b3bd3
ecountergg <ecountergg@gmail.com>
2025-05-09 23:17:26 +0700

    feat: add condition area options from user areas

--- composables/area/queries/useQueryAreaList.ts
+++ composables/area/queries/useQueryAreaList.ts
@@ -3,6 +3,7 @@ import type { AreaListResponse } from '~/models/Area';
 import type { AreaPaginationSearchParams } from '~/models/params/AreaPaginationSearchParams';
 import type { GenericPagination } from '~/types';
 import { useQuery } from '@tanstack/vue-query';
+import { USER_TYPE } from '~/constants';
 import { AreaServiceImpl } from '~/services/impl/AreaServiceImpl';
 
 export const useQueryAreaList = (
@@ -10,6 +11,7 @@ export const useQueryAreaList = (
     searchCount: MaybeRef<number>,
     options?: UseQueryOptions<GenericPagination<AreaListResponse[]>>,
 ) => {
+    const authStore = useAuthStore();
     const userService = new AreaServiceImpl();
     const query = useQuery<GenericPagination<AreaListResponse[]>>({
         queryKey: ['area-list', params, searchCount],
@@ -17,9 +19,31 @@ export const useQueryAreaList = (
         ...options,
     });
     const total = computed(() => Number(query.data.value?.elements ?? 0));
-    const results = computed(() =>
-        query.data.value && Array.isArray(query.data.value.data) ? query.data.value.data : [],
-    );
+    const results = computed(() => {
+        if (authStore.user?.userType === USER_TYPE.SUPER_ADMIN && query.data.value && Array.isArray(query.data.value.data)) {
+            return query.data.value?.data;
+        }
+        if (authStore.user?.userType !== USER_TYPE.SUPER_ADMIN && authStore.user) {
+            const areas: AreaListResponse[] = authStore.user.areas.map((area) => {
+                return {
+                    code: area,
+                    description: area,
+                    rates: [
+                        {
+                            tierStart: 0,
+                            tierEnd: 0,
+                            ratePerUnit: 0,
+                        },
+                    ],
+                };
+            });
+
+            return areas;
+        }
+
+        return [];
+    });
+
     const refetch = () => {
         if (params.page > 1)
             params.setFirstPage();
